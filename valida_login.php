fd9c6e45da76fd9b8dd601bc0de5f43123ce8929
Fabio-Frare <44546034+Fabio-Frare@users.noreply.github.com>
2024-10-30 21:40:45 -0300

    Navegação entre as páginas

--- valida_login.php
+++ valida_login.php
@@ -18,6 +18,7 @@ foreach($usuarios_app as $user) {
 
     if($usuario_autenticado) {
         $_SESSION['autenticado'] = 'SIM';
+        header('Location: home.php');
     } else {
         $_SESSION['autenticado'] = 'NAO';
         header('Location: index.php?login=erro');
